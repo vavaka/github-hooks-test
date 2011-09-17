78aeafa1034ba58932825d148a551f529e008dc6
Vladimir Kuznetsov <vavaka@gmail.com>
2011-09-17 10:16:10 +0000

    new string has been added to en.yml, DummyController has been added

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ end
 
 gem 'jquery-rails'
 
+gem 'execjs'
+gem 'therubyracer'
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
---
+++ test/functional/dummy_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DummyControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
